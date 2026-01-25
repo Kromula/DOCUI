bc7d68fb8fe68343d3b7d71e7b2af33c128cd54f
TheDoc <86298412+Kromula@users.noreply.github.com>
2026-01-25 15:19:10 +0000

    Added Shadow Priest, Ele Shaman and Prot Paladin Cooldown layouts

--- Addon/Profiles/CooldownManager.lua
+++ Addon/Profiles/CooldownManager.lua
@@ -17,7 +17,26 @@ DOCUI.Profiles.CooldownManager.layouts = {
         spec = "Balance",
         displayName = "Balance Druid",
         importString = [[1|HdC/L0NRFAdw57IYdCPfI0glmJR/AMN7FlJdRMNY7/UlkmppvN2jW+PHYBBb06nRVZpY28WvxOxHUmGwIn7E5H4tn9xz7zkn95xS13YFhWjneBFpnPR3yEqMVCzLv7hFHVV9bcCDjywClU4V4AlvKvsqB3hXMSpbOj+jsqdSU2mqtLRxpG6AUCWBJFIqrpoeNTE1S2zeR3rJABkkQ2SUjJFxkiATlvUpMs1wkozwdx/kh3ySL/KtzqE6Z+p2q1tG017lTi3355Z22fJQY32L7dIkTwpkg2yy4sJy1+bpmtyQK3LJFE6w+kxeGBYtj3N8rVeHo//BzC5SCJGMl+zKfATwogUnk8vkvWx8thiu+X8=]]
-    }
+    }.
+    {
+        class = "SHAMAN",
+        spec = "Elemental",
+        displayName = "Elemental Shaman",
+        importString = [[1|RdDZLkNRGAXgtmoergxdTSRH0kRCasr/R4tIRKsaQ4WICyVpEJUgiAdQ2sZMcOFWXHsFQ7LjgmfgRYx79YKb7+z8e5991llZ786NL5bZvZ6D9EMikAFIGNIDCUF6IVFIH2QIEoPEoYfQM8g4ZAyyBz2A7kMS0CtIFjICPYGeQo+g59AL6CUkB8n7Xe7uT8y+YfYdMgEZRnLRzsJB0moJvfpdnpYPLKzw6Bf5Jj+Yb+S225K+58pDikgxKSGlpIyU89wDeSRPnFWQSlJFqkkNqSV1pJ40EB/ZIJtki7wwZTNpI+2kg3SSLiR5uWEm44WM2uf2DHcCXN1xzJSGKQ1TGqY0jGUKbzLW8/JtU8b+rCf310ugUMl/Q06exdkSbYO2Rz2238pMRleXnKl0ai217gSdSGJwOv4L]]
+    },
+    {
+        class = "PALADIN",
+        spec = 'Protection',
+        displayName = "Protection Paladin",
+        importString = [[1|LdC9SgNRFATgGOzUVpitdmNphHkFUcSosOtf74Ioiiiod2OMP3ETEk2h2FlpNvEZ0ljmNWxjb5PaM5jmO7cYOOdOY7KWzaaj9G0RfADrcBUMpr3cxNQF3BXYAm/BBO4aTMEGkhnQ3hXwBnSgZWqKz4l18BW8A+/BMvgMVjHsg23wCYMv8AWuimTesnsrYlWUxJrYQPlXMxSR2BRbYht8tHncE+/iQ3yKjshEFz+Bl8vj0PAKIjaCojgQHZGJrvg2CqGIekFq621Lvj6+eNj3W/rzfwXWxbiFZjraic7PLv0oPon3j079BX85XNot/QE=]]
+    },
+    {
+        class = 'PRIEST',
+        spec = "Shadow",
+        displayName = "Shadow Priest",
+        importString = [[1|JdA/L0NRGAbw3qv+tSKxeSQn3gSJoTZFE41IhLb+tLS3VjfaBIObIGEgcZtSiZgMDAQRvbYOHRh8AB/C6ANYzLwPy++8Z3nOe55q+PihN+5XrrOQLkgUYveFrNsjcghpQ/8PYgVIGGLBjEE6YcZxAEgHBiIwkzATkG6sXeI+AWmHSWJ4FbF3lC5QntaYrTLcFz1rQzCjMAkdgyrvBTJL5kiKpEmGzJMFskiWSJbkyDJZIXnikCK5Iq/K3Y3yFCEB+VDqPYRP1hvkk7uAMD7QeHsqxOmEnJIaOSNv5Et5HlQa/EfTIlyyqUvayW3iKalvJRN9tHytza781yHnrOivA7hxSAjSAmlladqe9uYWfSe/4Za8fcntbJZ392REZrx1J/0L]]
+    }   
+    
 }
 
 -- Helper function to get class color
